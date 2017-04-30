fec368cf06bba4c5da55fdbd1b4690a047b141eb
Yuwei Zhou <yuwzho@microsoft.com>
2017-04-30 15:53:51 +0800

    :bug: fix the onclick handler

--- src/component/toolbar/toolbar.js
+++ src/component/toolbar/toolbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Glyphicon} from 'react-bootstrap';
+import AlertContainer from 'react-alert';
 import '../../common.css'
 import './toolbar.css';
 import IotHub from '../../lib/azure/iot-hub.js';
@@ -7,23 +8,31 @@ import IotHub from '../../lib/azure/iot-hub.js';
 class Banner extends Component {
     constructor(props) {
         super(props);
-        this.CONNECTION_STRING_PROMPT = 'Your Azure IoT hub device connection string';
-        this.DOWNLOAD_LINK = 'https://github.com/Azure-Samples/iot-hub-node-raspberrypi-client-app/archive/master.zip';
-    }
+        this.CONNECTION_STRING_PROMPT = 'Your Azure IoT hub device connection string'
+        this.state = {
+            downloadLink : 'https://github.com/Azure-Samples/iot-hub-node-raspberrypi-client-app/archive/master.zip'
+       }
+       this.connect = this.connect.bind(this);
+    
+    };
 
-    connect() {
-        var connstr = '';
-        this.hub = this.hub || new IotHub(connstr);
-        this.hub.connect();
-    }
+    connect (connstr) {
+        try {
+            this.hub = this.hub || new IotHub(connstr);
+            this.hub.connect();
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     render() {
+        const {DOWNLOAD_LINK} = this.state;
         return (
             <div className='toolbar'>
                 <div className='leftPanel'>
                     <span className='tool'><Glyphicon glyph='folder-open'/>Open Samples</span>
-                    <span className='tool' onClick={this.connect()}><Glyphicon glyph='play'/>Run app</span>
-                    <span className='tool'><a href={this.DOWNLOAD_LINK }><Glyphicon glyph='download-alt'/>Download</a></span>
+                    <span className='tool' onClick={this.connect}><Glyphicon glyph='play'/>Run app</span>
+                    <span className='tool'><a href={DOWNLOAD_LINK }><Glyphicon glyph='download-alt'/>Download</a></span>
                     <span className='tool'><Glyphicon glyph='share-alt'/>Share</span>
                 </div>
                 <div className='rightPanel connStrInputPanel'>
