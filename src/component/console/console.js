ec6e6d0fe17b94921b5c3759837ed6636984fc23
Yuwei Zhou <yuwzho@microsoft.com>
2017-05-09 20:22:11 +0800

    :books: Fix the welcome message

--- src/component/console/console.js
+++ src/component/console/console.js
@@ -3,7 +3,7 @@ import Console from 'react-console-component';
 import 'react-console-component/main.css';
 import './console.css';
 
-const welcomeMessage = 'Type `npm start` to run your app\nCurrently, we only allow edit once, but you can freely refresh the page since we keep your change';
+const welcomeMessage = 'Type `npm start` to run your app\nCurrently, we only allow run once, but you can freely refresh the page since we keep your change';
 
 class MyConsole extends Component {
   constructor(props) {
