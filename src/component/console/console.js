5d64a1ef9a6354401101d61cc4ac38cd419614aa
Yuwei Zhou <yuwzho@microsoft.com>
2017-05-10 19:00:53 +0800

    :books: modify console message

--- src/component/console/console.js
+++ src/component/console/console.js
@@ -3,7 +3,7 @@ import Console from 'react-console-component';
 import 'react-console-component/main.css';
 import './console.css';
 
-const welcomeMessage = 'Type `npm start` to run your app\nCurrently, we only allow run once, but you can freely refresh the page since we keep your change';
+const welcomeMessage = 'Type `npm start` to run your app\nCurrently.\nWe don\'t support stop the app, so you may need referesh the page to kill your thread.\nWe keep your changes to the editor even you referesh the page. You can click the \'reset\' to reset the code';
 
 class MyConsole extends Component {
   constructor(props) {
