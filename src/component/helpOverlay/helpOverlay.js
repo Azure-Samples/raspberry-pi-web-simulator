62a6befc7343fdca7df3c29ccfda1b6c5dda1073
Zhiqing Qiu <zhqqi@microsoft.com>
2017-06-09 18:06:17 +0800

    Add overlay multipage layout

--- src/component/helpOverlay/helpOverlay.js
+++ src/component/helpOverlay/helpOverlay.js
@@ -4,17 +4,65 @@ import closeButton from '../../img/closeButton.png';
 import connectionStringInPortal from '../../img/connectionStringInPortal.png';
 
 class HelpOverlay extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 0,
+      pageNum: 4
+    }
+  }
+  nextPage = ()=>{
+      this.setState((prev)=> {
+          return {
+              page: prev.page+1
+          }
+      })
+  }
+  prevPage = ()=>{
+      this.setState((prev)=> {
+          return {
+              page: prev.page-1
+          }
+      })
+  }
   render() {
     return (
-      <div className="overlay" style={{display: this.props.needShowHelp ? "block" : "none"}}>
-          <a className="closeButton" href="#" onClick={this.props.toggleHelpState}>
-              <img src={closeButton} width="40px" alt="close"/>
-          </a>
-          <div className="instruction">
-                In order to use simulator, you have to fill in IoT Hub device connection string in code editor<br/>
-                Open IoT Hub in Azure Portal, Click Device Explorer -> Choose a device (If there isn't device, create one) -> Copy connection string<br/>
-                Paste to code editor, replace '[Your IoT hub device connection string]'<br/>
-                <img src={connectionStringInPortal} width="950px" alt="get connection string from azure portal" />
+      <div className="overlay" style={{display: this.props.needShowHelp ? "flex" : "none"}}>
+          <div className="row1">
+            <a className="closeButton" href="#" onClick={this.props.toggleHelpState}>
+                <img src={closeButton} width="40px" alt="close"/>
+            </a>
+          </div>
+          <div className="row2">
+            <a className={`help-prev ${ this.state.page == 0 ? 'element-hide' : ''}`} onClick={this.prevPage}>&lt;</a>
+            <div className={`instruction ${ this.state.page == 0 ? 'element-show' : 'element-none'}`}>
+                    layout introduce
+            </div>
+            <div className={`instruction ${ this.state.page == 1 ? 'element-show' : 'element-none'}`}>
+                    In order to use simulator, you have to fill in IoT Hub device connection string in code editor<br/>
+                    Open IoT Hub in Azure Portal, Click Device Explorer -> Choose a device (If there isn't device, create one) -> Copy connection string<br/>
+                    Paste to code editor, replace '[Your IoT hub device connection string]'<br/>
+                    <img src={connectionStringInPortal} width="950px" alt="get connection string from azure portal" />
+            </div>
+            <div className={`instruction ${ this.state.page == 2 ? 'element-show' : 'element-none'}`}>
+                    In order to use simulator, you have to fill in IoT Hub device connection string in code editor<br/>
+                    Open IoT Hub in Azure Portal, Click Device Explorer -> Choose a device (If there isn't device, create one) -> Copy connection string<br/>
+                    Paste to code editor, replace '[Your IoT hub device connection string]'<br/>
+                    <img src={connectionStringInPortal} width="950px" alt="get connection string from azure portal" />
+            </div>
+            <div className={`instruction ${ this.state.page == 3 ? 'element-show' : 'element-none'}`}>
+                    In order to use simulator, you have to fill in IoT Hub device connection string in code editor<br/>
+                    Open IoT Hub in Azure Portal, Click Device Explorer -> Choose a device (If there isn't device, create one) -> Copy connection string<br/>
+                    Paste to code editor, replace '[Your IoT hub device connection string]'<br/>
+                    <img src={connectionStringInPortal} width="950px" alt="get connection string from azure portal" />
+            </div>
+            <a className={`help-next ${ this.state.page == 3 ? 'element-hide' : ''}`} onClick={this.nextPage}>&gt;</a>
+          </div>
+          <div className="row3">
+              <div className={`dot ${ this.state.page == 0 ? 'dot-selected' : ''}`} />
+              <div className={`dot ${ this.state.page == 1 ? 'dot-selected' : ''}`} />
+              <div className={`dot ${ this.state.page == 2 ? 'dot-selected' : ''}`} />
+              <div className={`dot ${ this.state.page == 3 ? 'dot-selected' : ''}`} />
           </div>
       </div>
     );
