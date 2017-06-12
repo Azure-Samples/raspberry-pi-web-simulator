de7aa6cdad55618e0f15538fd286107b1cc16d87
Zhiqing Qiu <zhqqi@microsoft.com>
2017-06-12 14:34:23 +0800

    Add slider animation

--- src/component/helpOverlay/helpOverlay.js
+++ src/component/helpOverlay/helpOverlay.js
@@ -9,16 +9,13 @@ class HelpOverlay extends Component {
     this.state = {
       page: 0,
       pageNum: 4,
-      pageClassNames: ["element-show","element-none","element-none","element-none"]
+      pageClassNames: ["element-show","element-show","element-show","element-show"]
     }
   }
   nextPage = ()=>{
       this.setState((prev)=> {
-          var tmpClassNames = prev.pageClassNames;
-          tmpClassNames[prev.page] = "slideLeftOut";
-          tmpClassNames[prev.page+1] = "slideRightIn";
           return {
-              pageClassNames: tmpClassNames,
+            //   pageClassNames: tmpClassNames,
               page: prev.page+1
           }
       })
@@ -40,26 +37,31 @@ class HelpOverlay extends Component {
           </div>
           <div className="row2">
             <a className={`help-prev ${ this.state.page == 0 ? 'element-hide' : ''}`} onClick={this.prevPage}>&lt;</a>
-            <div id="page0" className={`instruction ${this.state.pageClassNames[0]}`}>
-                    layout introduce
+            <div className="instruction-container" style={{transform:"translate("+this.state.page*-100+"%,0)"}}>
+            <div id="page0" className={`instruction element-show`}>
+                    [LAYOUT INTRODUCE]In order to use simulator, you have to fill in IoT Hub device connection string in code editor<br/>
+                    Open IoT Hub in Azure Portal, Click Device Explorer -> Choose a device (If there isn't device, create one) -> Copy connection string<br/>
+                    Paste to code editor, replace '[Your IoT hub device connection string]'<br/>
+                    <img src={connectionStringInPortal} width="950px" alt="get connection string from azure portal" />
             </div>
-            <div id="page1" className={`instruction ${this.state.pageClassNames[1]}`}>
+            <div id="page1" style={{left:"100%"}} className={`instruction element-show`}>
                     In order to use simulator, you have to fill in IoT Hub device connection string in code editor<br/>
                     Open IoT Hub in Azure Portal, Click Device Explorer -> Choose a device (If there isn't device, create one) -> Copy connection string<br/>
                     Paste to code editor, replace '[Your IoT hub device connection string]'<br/>
                     <img src={connectionStringInPortal} width="950px" alt="get connection string from azure portal" />
             </div>
-            <div id="page2" className={`instruction ${this.state.pageClassNames[2]}`}>
+            <div id="page2" style={{left:"200%"}} className={`instruction element-show`}>
                     In order to use simulator, you have to fill in IoT Hub device connection string in code editor<br/>
                     Open IoT Hub in Azure Portal, Click Device Explorer -> Choose a device (If there isn't device, create one) -> Copy connection string<br/>
                     Paste to code editor, replace '[Your IoT hub device connection string]'<br/>
                     <img src={connectionStringInPortal} width="950px" alt="get connection string from azure portal" />
             </div>
-            <div id="page3" className={`instruction ${this.state.pageClassNames[3]}`}>
+            <div id="page3" style={{left:"300%"}} className={`instruction element-show`}>
                     In order to use simulator, you have to fill in IoT Hub device connection string in code editor<br/>
                     Open IoT Hub in Azure Portal, Click Device Explorer -> Choose a device (If there isn't device, create one) -> Copy connection string<br/>
                     Paste to code editor, replace '[Your IoT hub device connection string]'<br/>
                     <img src={connectionStringInPortal} width="950px" alt="get connection string from azure portal" />
+            </div>
             </div>
             <a className={`help-next ${ this.state.page == 3 ? 'element-hide' : ''}`} onClick={this.nextPage}>&gt;</a>
           </div>
