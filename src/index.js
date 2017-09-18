73980ba90e855b9eb6d4d19f7a6c489de19fc322
Zhiqing Qiu <michaeljqzq@users.noreply.github.com>
2017-09-18 15:43:57 +0800

    Pop out a bubble when connection string is not provided (#29)

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ class Index extends Component {
       },
       LEDTurnOn: false,
       isRunning: false,
-      showHelp: false
+      showHelp: false,
     }
     if (typeof(Storage) !== "undefined") {
         var disableHelp = localStorage.getItem("disable-help");
@@ -134,6 +134,9 @@ class Index extends Component {
   onError(error) {
     let errorMap = ErrorMap.getInstance();
     if(errorMap.has(error)) {
+        if(error === 'The connection string is missing the property: HostName') {
+            this.showBubble();
+        }
         error = errorMap.get(error);
     }
     this.setState(function () {
@@ -141,9 +144,77 @@ class Index extends Component {
         console: {
           consoleErr: error.message || JSON.stringify(error),
           consoleErrTimeStamp: Date.now()
-        }
+        },
       };
     });
+  }
+
+  showBubble = () => {
+      let aTags = document.getElementsByTagName("span");
+      let searchText = "'[Your IoT hub device connection string]'";
+      let found;
+      for (var i = 0; i < aTags.length; i++) {
+          if (aTags[i].textContent == searchText) {
+              found = aTags[i];
+              break;
+          }
+      }
+      if(!found) {
+          return;
+      }
+      let bubble = document.createElement("div");
+      bubble.id = 'bubble1';
+      let tri = document.createElement("span");
+      tri.id = 'bubble2';
+      bubble.innerText = "Replace the placeholder with the Azure IoT hub";
+      let cs = document.createElement("div");
+      cs.innerText = "device connection string";
+      cs.style = `
+        font-weight: bold;
+      `;
+      bubble.appendChild(cs);
+      bubble.style = `
+        position: absolute;
+        left: ${found.offsetLeft + 30}px;
+        top: ${found.offsetTop + 40}px;
+        width: 220px;
+        background: #0078d7;
+        color: white;
+        height: 90px;
+        z-index: 10;
+        font-family: 'Segoe UI';
+        font-size: 15px;
+        padding: 10px 10px;
+        transition: all ease 0.3s;
+        text-align: center;
+        line-height: 22px;
+        border: 0 solid #666;`;
+      tri.style = `
+        position: absolute;
+        width: 0;
+        height: 0;
+        z-index: 10;
+        transition: all ease 0.3s;
+        left: ${found.offsetLeft + 45}px;
+        top: ${found.offsetTop + 10}px;
+        border: 15px solid;
+        border-color: transparent transparent #0078d7 transparent ;`;
+      let editor = document.getElementsByClassName('ace_content')[0];
+      editor.appendChild(tri);
+      editor.appendChild(bubble);
+      let listener = () => {
+        let element1 = document.getElementById("bubble1");
+        let element2 = document.getElementById("bubble2");
+        element1.style.opacity = 0;
+        element2.style.opacity = 0;
+        setTimeout(()=>{
+            element1.parentNode.removeChild(element1);
+            element2.parentNode.removeChild(element2);
+        },300);
+        
+        document.body.removeEventListener('mousedown',listener);
+      }
+      document.body.addEventListener('mousedown',listener);
   }
 
   toggleHelpState = () => {
